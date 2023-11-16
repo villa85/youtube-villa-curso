de4e062d4ac3b0d8081ef2c25bab863ee01fe4d8
villa <villalon2511@gmail.com>
2023-11-16 11:47:23 +0100

    variacion ejer 10 v16

---
+++ addons/10_cargar_datos_xml_rpc_v16/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Father MFH',
+    'version': '16.0.1.0.0',
+    'author': 'Yuniel Villalón',
+    'maintainer': 'Yuniel Villalón',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'summary': 'Short summary.',
+    'depends': ['base'],
+    'data': [
+        'views/view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+}
