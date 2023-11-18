136c4e1dd7d33206ce731974721375bd04ba2219
villa85 <villalon2511@gmail.com>
2023-11-18 14:20:41 +0100

    export excel partner (a medias)

---
+++ addons/16_export_partner_excel_v16/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Partner Export Excel',
+    'version': '16.0.1.0.0',
+    'author': 'Yuniel Villalón',
+    'maintainer': 'Yuniel Villalón',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'summary': 'Partner Export Excel',
+    'depends': ['stock'],
+    'data': [
+            'security/ir.model.access.csv',
+            'wizard/partner_export_excel_wzd.xml',
+            ],
+    'images': ['static/description/banner.jpg'],
+}
