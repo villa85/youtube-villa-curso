077cf6e5943b354de42f5b4b412330ee6f94c0c0
villa85 <villalon2511@gmail.com>
2023-11-22 21:48:58 +0100

    modulos pasados a la version 17, excepto sale_advance_permissions

---
+++ addons/sale_advance_permissions/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Sale Advance Permissions MFH',
+    'version': '17.0',
+    'author': 'Yuniel Villalón',
+    'maintainer': 'Yuniel Villalón',
+    'license': 'AGPL-3',
+    'category': 'Sale',
+    'summary': 'Sale Advance Permissions.',
+    'depends': ['sale_management'],
+    'data': [
+            'security/security.xml',
+            'views/sale_order_view.xml',
+            'views/product_view.xml',
+            ],
+    'images': ['static/description/icon.jpg'],
+}
---
+++ addons/wizard_from_action/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Modify Product MFH',
+    'version': '17.0',
+    'author': 'Marlon Falcon Hernandez',
+    'maintainer': 'Marlon Falcon Hernandez',
+    'website': 'http://www.marlonfalcon.com',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'summary': 'Modify Product.',
+    'depends': ['base','product','account'],
+    'data': [
+            'security/ir.model.access.csv',
+            'wizard/modify_product_wzd.xml',
+            ],
+    'images': ['static/description/banner.jpg'],
+}
